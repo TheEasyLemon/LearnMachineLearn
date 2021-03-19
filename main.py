75915eed9ef254ff5511a1157a8fd88a5060ccb1
Dawson Ren <dawsonren@gmail.com>
2021-03-19 12:55:25 -0500

    A fully functioning, but really really terrible model

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 ### yay!!!
 
 import numpy as np
-import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
 import csv
 
 from tensorflow.keras.layers.experimental.preprocessing import Normalization
@@ -11,14 +13,29 @@ from tensorflow.keras.layers.experimental.preprocessing import Normalization
 
 class WineRater:
     def __init__(self):
+        self.inputLabels = ["fixed acidity", "volatile acidity", "citric acid",
+                           "residual sugar", "chlorides", "free sulfur dioxide",
+                           "total sulfur dioxide", "density", "pH", "sulphates",
+                           "alcohol"]
+
         redData, whiteData = self.getWineQualityData()
         self.redWineData = redData
         self.whiteWineData = whiteData
         self.normalizeData()
 
+        redIO, whiteIO = self.splitData()
+        # "Red Training Inputs" = "rTrainI"
+        self.rTrainI, self.rTrainO, self.rTestI, self.rTestO = redIO
+        self.wTrainI, self.wTrainO, self.wTestI, self.wTestO = whiteIO
+
+        self.buildModel()
+        self.trainModel()
+        self.testModel()
+
+
     def getWineQualityData(self):
         """
-        Returns two numpy arrays that contain the data.
+        Returns two numpy arrays that contain the red/white wine data.
         """
         with open("winequality-red.csv") as file:
             # Ignore the first line since it's text
@@ -47,7 +64,53 @@ class WineRater:
                 normalized = normalizer(column)
                 array[:, i] = np.reshape(normalized, column.shape)
 
+    def splitData(self):
+        """
+        Split the data into training/testing sets, and also
+        split off the ratings (which is what we're trying
+        to predict).
+        Returns:
+        [[redTrainingInputs, redTrainingOutputs, redTestingInputs,
+        redTestingOutputs], [whiteTrainingInputs, whiteTrainingOutputs,
+        whiteTestingInputs, whiteTestingOutputs]]
+        """
+        values = [None, None]
+
+        for i, array in enumerate((self.redWineData, self.whiteWineData)):
+            # Split into two (halfway down the file)
+            training, testing = np.split(array, [array.shape[0] // 2], axis=0)
+
+            trainingInputs = training[:, :-1]
+            trainingOutputs = training[:, -1]
+            testingInputs = testing[:, :-1]
+            testingOutputs = testing[:, -1]
+
+            values[i] = [trainingInputs, trainingOutputs,
+                         testingInputs, testingOutputs]
+
+        return values
+
+    def buildModel(self):
+        self.model = Sequential()
+
+        # Create input layer
+        # Use Rectified Linear Unit (x > 0 ? 1 : 0)
+        self.model.add(Dense(15, input_dim=11, activation='relu'))
+        self.model.add(Dense(11, activation='relu'))
+        self.model.add(Dense(1, activation='sigmoid'))
+
+        self.model.compile(loss='binary_crossentropy',
+                           optimizer='adam',
+                           metrics=['accuracy'])
+
+    def trainModel(self):
+        self.model.fit(self.rTrainI, self.rTrainO, epochs=10, batch_size=100)
+        _, accuracy = self.model.evaluate(self.rTrainI, self.rTrainO)
+        print('Training Data Accuracy: %.2f' % (accuracy * 100))
+
+    def testModel(self):
+        _, accuracy = self.model.evaluate(self.rTestI, self.rTestO)
+        print('Testing Data Accuracy: %.2f' % (accuracy * 100))
 
 if __name__ == "__main__":
     wr = WineRater()
-    
