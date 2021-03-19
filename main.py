10840cfc3e44e8f7fa90e531c76b2bbc0166dd62
Ronik Bhaskar <ronikbhaskar@Roniks-Laptop.local>
2021-03-18 20:23:51 -0500

    totally did things

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 ### Ronik and Dawson's attempt at machine learning
 ### yay!!!
 
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import csv
+
 
 if __name__ == "__main__":
     return 1
