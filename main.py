84b0b6442f484054457be294fd6df0ecfff9d81e
Dawson Ren <dawsonren@gmail.com>
2021-03-25 16:36:32 -0500

    Add validation data

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import numpy as np
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-
-import csv
-
 from tensorflow.keras.layers.experimental.preprocessing import Normalization
 
 
@@ -21,21 +18,17 @@ class WineRater:
         redData, whiteData = self.getWineQualityData()
         self.redWineData = redData
         self.whiteWineData = whiteData
-        # self.normalizeData()
+        self.normalizeData()
 
         redIO, whiteIO = self.splitData()
         # "Red Training Inputs" = "rTrainI"
-        self.rTrainI, self.rTrainO, self.rTestI, self.rTestO = redIO
-        self.wTrainI, self.wTrainO, self.wTestI, self.wTestO = whiteIO
-
-        self.buildModel()
-        self.trainModel()
-        self.testModel()
+        self.rTrainI, self.rTrainO, self.rValI, self.rValO, self.rTestI, self.rTestO = redIO
+        self.wTrainI, self.wTrainO, self.wValI, self.wValO, self.wTestI, self.wTestO = whiteIO
 
 
     def getWineQualityData(self):
         """
-        Returns two numpy arrays that contain the red/white wine data.
+        Returns a tuple of numpy arrays that contain the red/white wine data.
         """
         with open("winequality-red.csv") as file:
             # Ignore the first line since it's text
@@ -55,6 +48,7 @@ class WineRater:
         variation of 1 and a mean of 0, as keras expects.
         """
         for array in (self.redWineData, self.whiteWineData):
+            # Transpose the matrix to iterate by column
             for i, column in enumerate(array.T):
                 # generate normalization function
                 normalizer = Normalization(axis=-1)
@@ -69,27 +63,31 @@ class WineRater:
         Split the data into training/testing sets, and also
         split off the ratings (which is what we're trying
         to predict).
-        Returns:
-        [[redTrainingInputs, redTrainingOutputs, redTestingInputs,
-        redTestingOutputs], [whiteTrainingInputs, whiteTrainingOutputs,
-        whiteTestingInputs, whiteTestingOutputs]]
+        Returns in the form:
+        [red [training validation, testing], white...]
 
-        Side effect is that the redWineData and whiteWineData is shuffled.
+        Side effect is that the redWineData and whiteWineData are shuffled.
         """
         values = [None, None]
 
         for i, array in enumerate((self.redWineData, self.whiteWineData)):
-            # Split into two (halfway down the file)
-            np.random.shuffle(array)
-            training, testing = np.split(array, [array.shape[0] // 2], axis=0)
+            # Splits the data into 70/10/20, training validation, testing
+            # np.random.shuffle(array)
+            entries = array.shape[0]
+            training, validation, testing = np.split(array,
+                                                    [round(entries * 7 / 10),
+                                                     round(entries * 8 / 10)],
+                                                    axis=0)
 
             trainingInputs = training[:, :-1]
             trainingOutputs = training[:, -1]
+            validationInputs = testing[:, :-1]
+            validationOutputs = testing[:, -1]
             testingInputs = testing[:, :-1]
             testingOutputs = testing[:, -1]
 
-            values[i] = [trainingInputs, trainingOutputs,
-                         testingInputs, testingOutputs]
+            values[i] = [trainingInputs, trainingOutputs, validationInputs,
+                         validationOutputs, testingInputs, testingOutputs]
 
         return values
 
@@ -98,7 +96,7 @@ class WineRater:
 
         # Create input layer
         # Use Rectified Linear Unit (x > 0 ? 1 : 0)
-        self.model.add(Dense(11, input_dim=11, activation='sigmoid'))
+        self.model.add(Dense(11, input_dim=11, activation='relu'))
         self.model.add(Dense(11, activation='sigmoid'))
         self.model.add(Dense(11, activation='sigmoid'))
         self.model.add(Dense(1, activation='sigmoid'))
@@ -108,7 +106,9 @@ class WineRater:
                            metrics=['accuracy'])
 
     def trainModel(self):
-        self.model.fit(self.rTrainI, self.rTrainO, epochs=150, batch_size=400)
+        self.model.fit(self.rTrainI, self.rTrainO,
+                       epochs=150, batch_size=400,
+                       validation_data=(self.rValI, self.rValO))
         _, accuracy = self.model.evaluate(self.rTrainI, self.rTrainO)
         print('Training Data Accuracy: %.2f' % (accuracy * 100))
 
@@ -118,3 +118,9 @@ class WineRater:
 
 if __name__ == "__main__":
     wr = WineRater()
+    wr.buildModel()
+    wr.trainModel()
+    wr.testModel()
+
+    print(wr.rTrainO[:10])
+    print(wr.rTestO[:10])
